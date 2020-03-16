63d6d2c7b57174141267f382c8e4f7c03bec082c
waitsj778 <jayhong778@gmail.com>
2020-03-16 21:33:10 +0900

    udpate

--- api/gets3.js
+++ api/gets3.js
@@ -7,7 +7,6 @@ var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
 var params = {
         Bucket: process.env.bucket,
-        // Key: process.env.profilepic,
         Key: process.env.samplepdf,
         Expires: 60
 };
