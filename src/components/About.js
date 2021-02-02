08c3814c6f99487b64f03bedc496dadb1401efaf
waitsj778 <jayhong778@gmail.com>
2021-02-02 22:22:19 +0900

    [mod] modify className for badge

--- src/components/About.js
+++ src/components/About.js
@@ -26,13 +26,13 @@ export default class About extends Component {
             <div className="three columns">
                <ScriptTag isHydration={true} type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer />
                {/* vertical badge */}
-               <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="ja_JP" data-type="vertical" data-theme="light" data-vanity="sanghyun-hong-8188b718b">
-                  <a class="LI-simple-link" href='https://jp.linkedin.com/in/sanghyun-hong-8188b718b?trk=profile-badge'>SANGHYUN HONG</a>
-               </div>
-               {/* horizontal badge */}
-               {/* <div class="LI-profile-badge"  data-version="v1" data-size="medium" data-locale="ja_JP" data-type="horizontal" data-theme="dark" data-vanity="sanghyun-hong-8188b718b">
+               {/* <div class="LI-profile-badge"  data-version="v1" data-width="180" data-size="medium" data-locale="ja_JP" data-type="vertical" data-theme="light" data-vanity="sanghyun-hong-8188b718b">
                   <a class="LI-simple-link" href='https://jp.linkedin.com/in/sanghyun-hong-8188b718b?trk=profile-badge'>SANGHYUN HONG</a>
                </div> */}
+               {/* horizontal badge */}
+               <div class="LI-profile-badge" className="profile-pic" data-version="v1" data-size="medium" data-locale="ja_JP" data-type="horizontal" data-theme="dark" data-vanity="sanghyun-hong-8188b718b">
+                  <a class="LI-simple-link" href='https://jp.linkedin.com/in/sanghyun-hong-8188b718b?trk=profile-badge'>SANGHYUN HONG</a>
+               </div>
                {/* profile image */}
                {/* <img className="profile-pic" src="../images/profilepic.jpg" alt="#"></img> */}
             </div>
